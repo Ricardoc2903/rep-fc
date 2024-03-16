f99280bd33763ef9bf11e8b004b0afac29340578
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-03-16 20:17:54 -0300

    cambios

--- prisma/migrations/20240316225538_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Form" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nombre" TEXT NOT NULL,
-    "number" INTEGER NOT NULL,
-    "email" TEXT NOT NULL,
-    "msg" TEXT NOT NULL
-);
