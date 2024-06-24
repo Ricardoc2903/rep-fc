ec8aaa69045c022c08b9479d4482d11bd9f72253
Ricardoc2903 <122412829+Ricardoc2903@users.noreply.github.com>
2024-06-24 18:38:17 -0300

    sacando la etiqueta meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ import FloatingBtn from "@/components/FloatingBtn"
 export const metadata: Metadata = {
   title: "FC Refrigeracion",
   description: "Pagina oficial de FC Refrigeracion",
-  name="google-site-verification" content="XSnsQL_0Jo6aMqmzwELNcpaOOMRHP2AQs4WbBfp0aNg",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
