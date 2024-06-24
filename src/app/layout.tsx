9cb6a93fc0592db1c65288f8058fb41f8baba0aa
Ricardoc2903 <122412829+Ricardoc2903@users.noreply.github.com>
2024-06-24 19:03:29 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="google-site-verification" content="XSnsQL_0Jo6aMqmzwELNcpaOOMRHP2AQs4WbBfp0aNg" />
+      </Head>
       <body>
         <Providers>
           <div>
