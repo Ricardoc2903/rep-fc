1b5f499bdae35dcf8358a2cf68bacadfed13af92
Ricardoc2903 <122412829+Ricardoc2903@users.noreply.github.com>
2024-06-26 15:11:34 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import FloatingBtn from "@/components/FloatingBtn"
 
 
 export const metadata: Metadata = {
+  application-name: "FC Refrigeracion",
   title: "FC Refrigeracion - Servicio Técnico Especializado de Refrigeración",
   description: "Página oficial de FC Refrigeración. Ofrecemos servicio técnico especializado.",
   keywords: "fc, refrigeracion, service, reparación de heladeras, instalacion de aire acondicionado, service de aire acondicionado, mantenimiento de aire acondicionado, service de heladeras, service de lavarropas",
