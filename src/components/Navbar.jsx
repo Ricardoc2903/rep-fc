2929f456262a2926bec860fae7dbfb0a439c128c
Ricardoc2903 <ricardodavidquevedo29@gmail.com>
2024-10-11 12:56:00 -0300

    cambio de tamaño del logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function App() {
           className="sm:hidden"
         />
         <NavbarBrand>
-          <Image src={logoSrc} className=" w-52 h-16 sm:h-14" alt="logo" />
+          <Image src={logoSrc} className=" w-36 h-10" alt="logo" />
         </NavbarBrand>
       </NavbarContent>
 
