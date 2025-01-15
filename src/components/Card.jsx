30979e46b6245dab40fc66525d52aca5265d0816
Ricardoc2903 <ricardodavidquevedo29@gmail.com>
2025-01-15 15:17:11 -0300

    añadiendo anuncio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { Card, CardHeader, CardBody } from "@nextui-org/react";
-
-import Image from 'next/image'
-
-import prueba from "@/images/mantenimiento1.jpg"
+import AdBanner from "./AdBanner";
+import Image from "next/image";
 
 export default function App({ imagen, titulo, mensaje }) {
   return (
     <div>
       <div className="card bg-gray-100 dark:bg-zinc-900 shadow-large shadow-zinc-900 w-72 mt-4 rounded-large h-full">
-        <Image src={imagen} alt={titulo} className="w-72 rounded-t-large"/>
+        <Image src={imagen} alt={titulo} className="w-72 rounded-t-large" />
         <div className="card-content flex justify-center align-middle flex-wrap p-2">
           <h3 className=" font-bold text-primary-400 text-lg">{titulo}</h3>
           <p>{mensaje}</p>
         </div>
       </div>
+      <AdBanner
+        dataAdFormat="auto"
+        dataFullWidthResponsive={true}
+        dataAdSlot="4326240930"
+      />
     </div>
   );
 }
